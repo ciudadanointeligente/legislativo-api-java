7b1ae56713282902de610782fa5fbf1f60bac177
Aldo Canepa <waldo.c@gmail.com>
2011-12-13 13:20:08 -0300

    Exceptions: Removed Serial unique ID
    Change-Id: Ic770795df43a0999f7cca787fea492280d9e3e19

--- src/main/java/cl/votainteligente/legislativo/exceptions/ResourceNotFoundException.java
+++ src/main/java/cl/votainteligente/legislativo/exceptions/ResourceNotFoundException.java
@@ -5,9 +5,4 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-
-	/**
-	 *
-	 */
-	private static final long serialVersionUID = -1047678371267298618L;
 }
