5f19ab99abbdd4748c6674d6f0562981f1f75be0
rrebolle <rod.rebo@gmail.com>
2011-12-16 18:45:03 -0300

    Added MergedBill to model merged bills
    Change-Id: Ic4e35cc68c061fc1f14f773657847b086597f45d

---
+++ src/main/java/cl/votainteligente/legislativo/model/MergedBill.java
@@ -0,0 +1,68 @@
+package cl.votainteligente.legislativo.model;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "merged_bill")
+public class MergedBill {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@OneToOne
+	@JoinColumn(name="new_bill_id")
+	private Bill newBill;
+
+	@OneToMany
+	@JoinColumn(name = "merged_bills_id")
+	private Set<Bill> mergedBills;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "merged_date", nullable = false)
+	private Date mergedDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Bill getNewBill() {
+		return newBill;
+	}
+
+	public void setNewBill(Bill newBill) {
+		this.newBill = newBill;
+	}
+
+	public Set<Bill> getMergedBills() {
+		return mergedBills;
+	}
+
+	public void setMergedBills(Set<Bill> mergedBills) {
+		this.mergedBills = mergedBills;
+	}
+
+	public Date getMergedDate() {
+		return mergedDate;
+	}
+
+	public void setMergedDate(Date mergedDate) {
+		this.mergedDate = mergedDate;
+	}
+}
