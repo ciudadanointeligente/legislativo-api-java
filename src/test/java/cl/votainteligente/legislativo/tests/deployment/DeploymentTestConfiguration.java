41ca4997d0a9aae6a32b36643d8ab672d87217c2
Jose Gonzalez Quintana <jgonzalezq@votainteligente.cl>
2012-02-13 18:54:55 -0300

    Normalizes project config files
    Change-Id: I530f7b81877fbbffba53a92171925f32e981d3fc

--- src/test/java/cl/votainteligente/legislativo/tests/deployment/DeploymentTestConfiguration.java
+++ src/test/java/cl/votainteligente/legislativo/tests/deployment/DeploymentTestConfiguration.java
@@ -1,7 +1,7 @@
 package cl.votainteligente.legislativo.tests.deployment;
 
 public class DeploymentTestConfiguration {
-	public static final String webroot = "http://localhost:8983/legislativo/api/";
+	public static final String webroot = "http://localhost:8983/Legislativo/api/";
 
 	public static final String apiRoute(String api) {
 		return webroot + api;
