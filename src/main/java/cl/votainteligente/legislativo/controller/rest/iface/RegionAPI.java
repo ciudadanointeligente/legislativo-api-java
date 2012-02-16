32339e69f76e91c9129df21260c9167a7d19e57e
Jose Gonzalez Quintana <jgonzalezq@votainteligente.cl>
2012-02-16 13:22:47 -0300

    RegionController: Normalizes code
    Change-Id: Ib7fbfd1f381dd902177f9ac080dcf124d6f3da2b

--- src/main/java/cl/votainteligente/legislativo/controller/rest/iface/RegionAPI.java
+++ src/main/java/cl/votainteligente/legislativo/controller/rest/iface/RegionAPI.java
@@ -10,57 +10,44 @@ public interface RegionAPI {
 
 	/**
 	 *
-	 * @param page
-	 *            The number of the desired page to be retrieved.
-	 * @param perPage
-	 *            Amount of regions to be retrieved in a page.
+	 * @param page The number of the desired page to be retrieved.
+	 * @param perPage Amount of regions to be retrieved in a page.
 	 * @return A Page of Region (Region) <br />
-	 *         For Example:
-	 *
-	 *         <a href=
-	 *         "http://demo.ciudadanointeligente.cl/Legislativo/api/geo/region/all"
-	 *         >geo/region/all</a>
+	 * 			For Example:
+	 * 			<a href="http://demo.ciudadanointeligente.cl/Legislativo/api/region/all">region/all</a>
 	 * @see Region
 	 */
 	@Path("all")
 	@GET
-	Page<Region> getAll(@PathParam("page") @DefaultValue("1") final int page,
+	Page<Region> getAll(
+			@PathParam("page") @DefaultValue("1") final int page,
 			@PathParam("perPage") @DefaultValue("10") final int perPage);
 
 	/**
 	 * Returns all the Regions with a specific name.
 	 *
-	 * @param name
-	 *            The Region name to look for.
-	 * @param page
-	 *            The number of desired page to be retrieved.
-	 * @param perPage
-	 *            Amount of regions to be retrieved in a page.
+	 * @param name Region name to look for.
+	 * @param page The number of desired page to be retrieved.
+	 * @param perPage Amount of regions to be retrieved in a page.
 	 * @return A page of Region (Region) <br />
-	 *         For Example:
-	 *
-	 *         <a href=
-	 *         "http://demo.ciudadanointeligente.cl/Legislativo/api/geo/region/any?name=Magallanes"
-	 *         >geo/region/any?name=Magallanes</a>
+	 * 			For Example:
+	 * 			<a href="http://demo.ciudadanointeligente.cl/Legislativo/api/region/any?name=Magallanes">region/any?name=Magallanes</a>
 	 * @see Region
 	 */
 	@Path("any")
 	@GET
-	Page<Region> findRegionsByName(@PathParam("name") final String name,
+	Page<Region> findRegionsByName(
+			@PathParam("name") final String name,
 			@PathParam("page") @DefaultValue("1") final int page,
 			@PathParam("perPage") @DefaultValue("10") final int perPage);
 
 	/**
 	 * Allows you to get all the information of a Region.
 	 *
-	 * @param id
-	 *            The Region identification number.
+	 * @param id Region identification number.
 	 * @return Detailed of a Region <br />
-	 *         For Example:
-	 *
-	 *         <a href=
-	 *         "http://demo.ciudadanointeligente.cl/Legislativo/api/geo/region/any?id=1"
-	 *         >geo/region/any?id=1</a>
+	 * 			For Example:
+	 * 			<a href="http://demo.ciudadanointeligente.cl/Legislativo/api/region/any?id=1">region/any?id=1</a>
 	 * @throws ResourceNotFoundException
 	 * @see Region
 	 */
