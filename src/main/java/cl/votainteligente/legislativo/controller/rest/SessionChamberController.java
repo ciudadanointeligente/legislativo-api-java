46a2f07d24ebe070602dac88d305cf7c4a440000
Jose Gonzalez Quintana <jgonzalezq@votainteligente.cl>
2012-02-16 13:28:48 -0300

    SessionChamberController: Normalizes code
    Change-Id: I106ba477b2d714d2f1c84598c4c19e114e430577

--- src/main/java/cl/votainteligente/legislativo/controller/rest/SessionChamberController.java
+++ src/main/java/cl/votainteligente/legislativo/controller/rest/SessionChamberController.java
@@ -25,16 +25,14 @@ import javax.ws.rs.Path;
 @Path("sessionChamber")
 @Controller
 public class SessionChamberController implements SessionChamberAPI {
-	private SimpleDateFormat dateFormat = new SimpleDateFormat(
-			ApplicationProperties.getProperty("controller.date.format"));
+	private SimpleDateFormat dateFormat = new SimpleDateFormat(ApplicationProperties.getProperty("controller.date.format"));
+
 	@Autowired
-	SessionChamberService service;
+	SessionChamberService sessionChamberService;
 
 	/*
 	 * (non-Javadoc)
-	 *
-	 * @see
-	 * cl.votainteligente.legislativo.controller.rest.SessionAI#getAll(int,int)
+	 * @see cl.votainteligente.legislativo.controller.rest.SessionAI#getAll(int, int)
 	 */
 	@RequestMapping(value = "sessionChamber/all", method = RequestMethod.GET)
 	@ResponseBody
@@ -42,7 +40,7 @@ public class SessionChamberController implements SessionChamberAPI {
 			@RequestParam(value = "page", defaultValue = ApplicationProperties.CONTROLLER_PAGE_DEFAULT_VALUE, required = false) final int page,
 			@RequestParam(value = "perPage", defaultValue = ApplicationProperties.CONTROLLER_PER_PAGE_DEFAULT_VALUE, required = false) final int perPage) {
 		try {
-			return service.getAllSessionChamberDO(page, perPage);
+			return sessionChamberService.getAllSessionChamberDO(page, perPage);
 		} catch (ServiceException e) {
 			e.printStackTrace();
 			throw new ServerErrorException();
@@ -51,19 +49,19 @@ public class SessionChamberController implements SessionChamberAPI {
 
 	/*
 	 * (non-Javadoc)
-	 *
-	 * @see
-	 * cl.votainteligente.legislativo.controller.rest.SessionAI#getById(long)
+	 * @see cl.votainteligente.legislativo.controller.rest.SessionAI#getById(long)
 	 */
 	@RequestMapping(value = "sessionChamber/any", method = RequestMethod.GET)
 	@ResponseBody
-	public final SessionChamberDetailedDO getById(
-			@RequestParam(value = "id", required = true) final long id) {
+	public final SessionChamberDetailedDO getById(@RequestParam(value = "id", required = true) final long id) {
 		try {
-			SessionChamber s = service.getSessionChamber(id);
-			if (s == null)
+			SessionChamber sessionChamber = sessionChamberService.getSessionChamber(id);
+
+			if (sessionChamber == null) {
 				throw new ResourceNotFoundException();
-			return s.asSessionChamberDetailedDomainObject();
+			}
+
+			return sessionChamber.asSessionChamberDetailedDomainObject();
 		} catch (ServiceException e) {
 			e.printStackTrace();
 			throw new ServerErrorException();
@@ -71,18 +69,22 @@ public class SessionChamberController implements SessionChamberAPI {
 
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see cl.votainteligente.legislativo.controller.rest.SessionAI#getDateRange(String, String, int, int)
+	 */
 	@Override
 	@RequestMapping(value = "sessionChamber/dateRange", method = RequestMethod.GET)
 	@ResponseBody
 	public Page<SessionChamberDO> getDateRange(
-			@RequestParam(value = "from", required = true) final String fromString,
-			@RequestParam(value = "to", required = true) final String toString,
+			@RequestParam(value = "from", required = true) final String from,
+			@RequestParam(value = "to", required = true) final String to,
 			@RequestParam(value = "page", defaultValue = ApplicationProperties.CONTROLLER_PAGE_DEFAULT_VALUE, required = false) final int page,
 			@RequestParam(value = "perPage", defaultValue = ApplicationProperties.CONTROLLER_PER_PAGE_DEFAULT_VALUE, required = false) final int perPage) {
 		try {
-			Date from = dateFormat.parse(fromString);
-			Date to = dateFormat.parse(toString);
-			return service.getByDateRange(from, to, page, perPage);
+			Date startDate = dateFormat.parse(from);
+			Date endDate = dateFormat.parse(to);
+			return sessionChamberService.getByDateRange(startDate, endDate, page, perPage);
 		} catch (ServiceException e) {
 			e.printStackTrace();
 			throw new ServerErrorException();
@@ -91,6 +93,10 @@ public class SessionChamberController implements SessionChamberAPI {
 		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see cl.votainteligente.legislativo.controller.rest.SessionAI#getByLegislature(long, int, int)
+	 */
 	@Override
 	@RequestMapping(value = "sessionChamber/legislature", method = RequestMethod.GET)
 	@ResponseBody
@@ -99,7 +105,7 @@ public class SessionChamberController implements SessionChamberAPI {
 			@RequestParam(value = "page", defaultValue = ApplicationProperties.CONTROLLER_PAGE_DEFAULT_VALUE, required = false) final int page,
 			@RequestParam(value = "perPage", defaultValue = ApplicationProperties.CONTROLLER_PER_PAGE_DEFAULT_VALUE, required = false) final int perPage) {
 		try {
-			return service.getByLegislature(id, page, perPage);
+			return sessionChamberService.getByLegislature(id, page, perPage);
 		} catch (ServiceException e) {
 			throw new ServerErrorException();
 		}
--- src/main/java/cl/votainteligente/legislativo/controller/rest/iface/SessionChamberAPI.java
+++ src/main/java/cl/votainteligente/legislativo/controller/rest/iface/SessionChamberAPI.java
@@ -1,27 +1,21 @@
 package cl.votainteligente.legislativo.controller.rest.iface;
 
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-
 import cl.votainteligente.legislativo.common.Page;
 import cl.votainteligente.legislativo.model.DO.SessionChamberDO;
 import cl.votainteligente.legislativo.model.DO.SessionChamberDetailedDO;
+
+import javax.ws.rs.*;
 
 public interface SessionChamberAPI {
 
 	/**
 	 * Returns all the sessions.
 	 *
-	 * @param page
-	 *            The number of the desired page to be retrieved (optional).
-	 * @param perPage
-	 *            Amount of sessions to be retrieved in a page (optional).
+	 * @param page The number of the desired page to be retrieved (optional).
+	 * @param perPage Amount of sessions to be retrieved in a page (optional).
 	 * @return A Page of Session Abstracts (SessionChamberDO) <br />
-	 *         For example: <a href=
-	 *         "http://demo.ciudadanointeligente.cl/Legislativo/api/sessionChamber/all?page=1&perPage=10"
-	 *         >sessionChamber/all?page=1&perPage=10</a>
+	 * 			For example:
+	 * 			<a href="http://demo.ciudadanointeligente.cl/Legislativo/api/sessionChamber/all?page=1&perPage=10">sessionChamber/all?page=1&perPage=10</a>
 	 * @see SessionChamberDO
 	 */
 	@Path("all")
@@ -33,12 +27,10 @@ public interface SessionChamberAPI {
 	/**
 	 * Allows you to get all the information of a session.
 	 *
-	 * @param id
-	 *            The Session identification number.
+	 * @param id Session identification number.
 	 * @return Detailed of a session <br />
-	 *         For example: <a href=
-	 *         "http://demo.ciudadanointeligente.cl/Legislativo/api/sessionChamber/any?id=1"
-	 *         >sessionChamber/any?id=1</a>
+	 * 			For example:
+	 * 			<a href="http://demo.ciudadanointeligente.cl/Legislativo/api/sessionChamber/any?id=1">sessionChamber/any?id=1</a>
 	 * @see SessionChamberDetailedDO
 	 */
 	@Path("any")
@@ -48,47 +40,38 @@ public interface SessionChamberAPI {
 	/**
 	 * Returns all the sessions between two dates.
 	 *
-	 * @param fromString
-	 *            Beggining of the interval, in date format (dd-mm-yyyy).
-	 * @param toString
-	 *            End of the interval, in date format (dd-mm-yyyy).
-	 * @param page
-	 *            The number of the desired page to be retrieved (optional).
-	 * @param perPage
-	 *            Amount of sessions to be retrieved in a page (optional).
+	 * @param from Beggining of the interval, in date format (dd-mm-yyyy).
+	 * @param to End of the interval, in date format (dd-mm-yyyy).
+	 * @param page The number of the desired page to be retrieved (optional).
+	 * @param perPage Amount of sessions to be retrieved in a page (optional).
 	 * @return A Page of Session Abstracts (SessionChamberDO) <br />
-	 *         For example: <br />
-	 *         For example: <a href=
-	 *         "http://demo.ciudadanointeligente.cl/Legislativo/api/sessionChamber/dateRange?from=01-01-1990&to=01-01-2012&page=1&perPage=10"
-	 *         >sessionChamber/dateRange?from=01-01-1990&to=01-01-2012&page=1&perPage=
-	 *         10</a>
+	 * 			For example:
+	 * 			<a href="http://demo.ciudadanointeligente.cl/Legislativo/api/sessionChamber/dateRange?from=01-01-1990&to=01-01-2012&page=1&perPage=10">sessionChamber/dateRange?from=01-01-1990&to=01-01-2012&page=1&perPage=10</a>
 	 * @see SessionChamberDO
 	 */
 	@Path("dateRange")
 	@GET
-	Page<SessionChamberDO> getDateRange(@PathParam("from") final String fromString,
-			@PathParam("to") final String toString,
+	Page<SessionChamberDO> getDateRange(
+			@PathParam("from") final String from,
+			@PathParam("to") final String to,
 			@PathParam("page") @DefaultValue("1") final int page,
 			@PathParam("perPage") @DefaultValue("10") final int perPage);
 
 	/**
 	 * Returns all the sessions by Legislature number.
 	 *
-	 * @param legislatureId
-	 *            The legislatureId of the Legislature.
-	 * @param page
-	 *            The number of the desired page to be retrieved (optional).
-	 * @param perPage
-	 *            Amount of sessions to be retrieved in a page (optional).
+	 * @param id Legislature indentification number.
+	 * @param page The number of the desired page to be retrieved (optional).
+	 * @param perPage Amount of sessions to be retrieved in a page (optional).
 	 * @return A Page of Session Abstracts (SessionChamberDO) <br />
-	 *         For example: <a href=
-	 *         "http://demo.ciudadanointeligente.cl/Legislativo/api/sessionChamber/legislature?id=1&page=1&perPage=10"
-	 *         >sessionChamber/legislature?id=1&page=1&perPage=10</a>
+	 * 			For example:
+	 * 			<a href="http://demo.ciudadanointeligente.cl/Legislativo/api/sessionChamber/legislature?id=1&page=1&perPage=10">sessionChamber/legislature?id=1&page=1&perPage=10</a>
 	 * @see SessionChamberDO
 	 */
 	@Path("legislature")
 	@GET
-	Page<SessionChamberDO> getByLegislature(@PathParam("id") final long legislatureId,
+	Page<SessionChamberDO> getByLegislature(
+			@PathParam("id") final long id,
 			@PathParam("page") @DefaultValue("1") final int page,
 			@PathParam("perPage") @DefaultValue("10") final int perPage);
 }
