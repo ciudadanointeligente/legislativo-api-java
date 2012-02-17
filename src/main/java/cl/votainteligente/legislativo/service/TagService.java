12dc879d5af82c119592e1d6bb51a41138499f80
Jose Gonzalez Quintana <jgonzalezq@votainteligente.cl>
2012-02-17 16:55:18 -0300

    TagService: Normalizes code
    Change-Id: Iddffda2481620dbaebdac0d72afc2fb875d6626f

--- src/main/java/cl/votainteligente/legislativo/service/TagService.java
+++ src/main/java/cl/votainteligente/legislativo/service/TagService.java
@@ -6,9 +6,6 @@ import cl.votainteligente.legislativo.model.Tag;
 
 public interface TagService {
 	Tag getTag(Long id) throws ServiceException;
-
 	Page<Tag> getAll(int page, int perPage) throws ServiceException;
-
-	Page<Tag> findByName(String name, int page, int perPage)
-			throws ServiceException;
+	Page<Tag> findByName(String name, int page, int perPage) throws ServiceException;
 }
