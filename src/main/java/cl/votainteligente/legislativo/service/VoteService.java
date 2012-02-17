ba92021fd1dd65d2a75a7c17afd0e18942c1a91f
Jose Gonzalez Quintana <jgonzalezq@votainteligente.cl>
2012-02-17 16:56:33 -0300

    VoteService: Normalizes code
    Change-Id: I9ef58c911a58723dad1140ca43bae94d493daf07

--- src/main/java/cl/votainteligente/legislativo/service/VoteService.java
+++ src/main/java/cl/votainteligente/legislativo/service/VoteService.java
@@ -8,16 +8,9 @@ import cl.votainteligente.legislativo.model.DO.VoteDO;
 import cl.votainteligente.legislativo.model.DO.VoteDetailedDO;
 
 public interface VoteService {
-
 	Page<VoteDO> getAllVoteDO(int page, int perPage) throws ServiceException;
-
-	Page<VoteDO> getBySession(Session session, int page, int perPage)
-			throws ServiceException;
-
-	Page<VoteDO> getByResult(Long result, int page, int perPage)
-			throws ServiceException;
-
+	Page<VoteDO> getBySession(Session session, int page, int perPage) throws ServiceException;
+	Page<VoteDO> getByResult(Long result, int page, int perPage) throws ServiceException;
 	Vote getVote(Long id) throws ServiceException;
-
 	VoteDetailedDO getVoteDetailedDO(Long id) throws ServiceException;
 }
