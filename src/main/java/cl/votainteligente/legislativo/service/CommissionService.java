36abd442aea36ac2c3c94541da9b42457d30a4ba
Jose Gonzalez Quintana <jgonzalezq@votainteligente.cl>
2012-02-17 12:28:57 -0300

    CommissionService: Normalizes code
    Change-Id: I36546877e527b1008e5581cf6c859339403429bb

--- src/main/java/cl/votainteligente/legislativo/service/CommissionService.java
+++ src/main/java/cl/votainteligente/legislativo/service/CommissionService.java
@@ -9,26 +9,12 @@ import cl.votainteligente.legislativo.model.DO.CommissionDO;
 import cl.votainteligente.legislativo.model.DO.CommissionDetailedDO;
 
 public interface CommissionService {
-
 	Commission newCommission(Commission commission) throws ServiceException;
-
-	Page<CommissionDO> getAllCommissions(int pageNumber, int resultsPerPage)
-			throws ServiceException;
-
-	Page<CommissionDO> findCommissionsByName(String name, int pageNumber,
-			int resultsPerPage) throws ServiceException;
-
+	Page<CommissionDO> getAllCommissions(int pageNumber, int resultsPerPage) throws ServiceException;
+	Page<CommissionDO> findCommissionsByName(String name, int pageNumber, int resultsPerPage) throws ServiceException;
 	Commission getCommissionById(Long id) throws ServiceException;
-
-	Page<CommissionDO> getCommissionsByType(CommissionType commissionType,
-			int pageNumber, int resultsPerPage);
-
-	Page<CommissionDO> getCommissionsByChamber(Chamber chamber, int pageNumber,
-			int resultsPerPage);
-
-	CommissionDetailedDO getCommissionDetailedDO(Long id)
-			throws ServiceException;
-
+	Page<CommissionDO> getCommissionsByType(CommissionType commissionType, int pageNumber, int resultsPerPage) throws ServiceException;
+	Page<CommissionDO> getCommissionsByChamber(Chamber chamber, int pageNumber, int resultsPerPage) throws ServiceException;
+	CommissionDetailedDO getCommissionDetailedDO(Long id) throws ServiceException;
 	CommissionDO getCommissionDO(Long id) throws ServiceException;
-
 }
--- src/main/java/cl/votainteligente/legislativo/service/impl/CommissionServiceImpl.java
+++ src/main/java/cl/votainteligente/legislativo/service/impl/CommissionServiceImpl.java
@@ -1,12 +1,5 @@
 package cl.votainteligente.legislativo.service.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.Query;
-
-import org.springframework.stereotype.Service;
-
 import cl.votainteligente.legislativo.common.Page;
 import cl.votainteligente.legislativo.exception.ServiceException;
 import cl.votainteligente.legislativo.model.Chamber;
@@ -17,50 +10,51 @@ import cl.votainteligente.legislativo.model.DO.CommissionDetailedDO;
 import cl.votainteligente.legislativo.service.CommissionService;
 import cl.votainteligente.legislativo.service.EntityManagerService;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Query;
+
 @Service
-public class CommissionServiceImpl extends EntityManagerService implements
-		CommissionService {
+public class CommissionServiceImpl extends EntityManagerService implements CommissionService {
 
 	@Override
-	public Commission newCommission(Commission commission)
-			throws ServiceException {
+	public Commission newCommission(Commission commission) throws ServiceException {
 		getEntityManager().persist(commission);
 		return commission;
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
-	public Page<CommissionDO> getAllCommissions(int pageNumber,
-			int resultsPerPage) throws ServiceException {
-		Query query = getEntityManager().createQuery(
-				"select c from Commission c");
+	public Page<CommissionDO> getAllCommissions(int pageNumber, int resultsPerPage) throws ServiceException {
+		Query query = getEntityManager().createQuery("select c from Commission c");
 		long totalCommissions = query.getResultList().size();
 		query.setFirstResult((pageNumber - 1) * resultsPerPage);
 		query.setMaxResults(resultsPerPage);
-		List<CommissionDO> listDO = new ArrayList<CommissionDO>();
+		List<CommissionDO> commissionDOList = new ArrayList<CommissionDO>();
+
 		for (Commission commission : (List<Commission>) query.getResultList()) {
-			listDO.add((CommissionDO)commission.asDomainObject());
+			commissionDOList.add((CommissionDO) commission.asDomainObject());
 		}
-		return new Page<CommissionDO>(listDO, pageNumber, resultsPerPage,
-				totalCommissions);
+
+		return new Page<CommissionDO>(commissionDOList, pageNumber, resultsPerPage, totalCommissions);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
-	public Page<CommissionDO> findCommissionsByName(String name,
-			int pageNumber, int resultsPerPage) throws ServiceException {
-		Query query = getEntityManager().createQuery(
-				"select c from Commission c where upper(c.name) like upper(?)");
+	public Page<CommissionDO> findCommissionsByName(String name, int pageNumber, int resultsPerPage) throws ServiceException {
+		Query query = getEntityManager().createQuery("select c from Commission c where upper(c.name) like upper(?)");
 		query.setParameter(1, "%" + name + "%");
 		long totalCommissions = query.getResultList().size();
 		query.setFirstResult((pageNumber - 1) * resultsPerPage);
 		query.setMaxResults(resultsPerPage);
-		List<CommissionDO> listDO = new ArrayList<CommissionDO>();
+		List<CommissionDO> commissionDOList = new ArrayList<CommissionDO>();
+
 		for (Commission commission : (List<Commission>) query.getResultList()) {
-			listDO.add((CommissionDO)commission.asDomainObject());
+			commissionDOList.add((CommissionDO) commission.asDomainObject());
 		}
-		return new Page<CommissionDO>(listDO, pageNumber, resultsPerPage,
-				totalCommissions);
+
+		return new Page<CommissionDO>(commissionDOList, pageNumber, resultsPerPage, totalCommissions);
 	}
 
 	@Override
@@ -68,51 +62,45 @@ public class CommissionServiceImpl extends EntityManagerService implements
 		return getEntityManager().find(Commission.class, id);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
-	public Page<CommissionDO> getCommissionsByType(
-			CommissionType commissionType, int pageNumber, int resultsPerPage) {
-		Query query = getEntityManager().createQuery(
-				"select c from Commission c where c.commissionType = ?");
+	public Page<CommissionDO> getCommissionsByType(CommissionType commissionType, int pageNumber, int resultsPerPage) throws ServiceException {
+		Query query = getEntityManager().createQuery("select c from Commission c where c.commissionType = ?");
 		query.setParameter(1, commissionType);
 		long totalCommissions = query.getResultList().size();
 		query.setFirstResult((pageNumber - 1) * resultsPerPage);
 		query.setMaxResults(resultsPerPage);
-		List<CommissionDO> listDO = new ArrayList<CommissionDO>();
+		List<CommissionDO> commissionDOList = new ArrayList<CommissionDO>();
+
 		for (Commission commission : (List<Commission>) query.getResultList()) {
-			listDO.add((CommissionDO)commission.asDomainObject());
+			commissionDOList.add((CommissionDO) commission.asDomainObject());
 		}
-		return new Page<CommissionDO>(listDO, pageNumber, resultsPerPage,
-				totalCommissions);
+
+		return new Page<CommissionDO>(commissionDOList, pageNumber, resultsPerPage, totalCommissions);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
-	public Page<CommissionDO> getCommissionsByChamber(Chamber chamber,
-			int pageNumber, int resultsPerPage) {
-		Query query = getEntityManager().createQuery(
-				"select c from Commission c where c.chamber = ?");
+	public Page<CommissionDO> getCommissionsByChamber(Chamber chamber, int pageNumber, int resultsPerPage) throws ServiceException {
+		Query query = getEntityManager().createQuery("select c from Commission c where c.chamber = ?");
 		query.setParameter(1, chamber);
 		long totalCommissions = query.getResultList().size();
 		query.setFirstResult((pageNumber - 1) * resultsPerPage);
 		query.setMaxResults(resultsPerPage);
-		List<CommissionDO> listDO = new ArrayList<CommissionDO>();
+		List<CommissionDO> commissionDOList = new ArrayList<CommissionDO>();
+
 		for (Commission commission : (List<Commission>) query.getResultList()) {
-			listDO.add((CommissionDO)commission.asDomainObject());
+			commissionDOList.add((CommissionDO) commission.asDomainObject());
 		}
-		return new Page<CommissionDO>(listDO, pageNumber, resultsPerPage,
-				totalCommissions);
+
+		return new Page<CommissionDO>(commissionDOList, pageNumber, resultsPerPage, totalCommissions);
 	}
 
 	@Override
-	public CommissionDetailedDO getCommissionDetailedDO(Long id)
-			throws ServiceException {
+	public CommissionDetailedDO getCommissionDetailedDO(Long id) throws ServiceException {
 		return new CommissionDetailedDO(getCommissionById(id));
 	}
 
 	@Override
 	public CommissionDO getCommissionDO(Long id) throws ServiceException {
-		return (CommissionDO)getCommissionById(id).asDomainObject();
+		return (CommissionDO) getCommissionById(id).asDomainObject();
 	}
-
 }
