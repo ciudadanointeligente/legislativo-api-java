63db80e1769d3ea4bd1795b20ce03fe1608459c4
Jose Gonzalez Quintana <jgonzalezq@votainteligente.cl>
2012-02-17 16:10:56 -0300

    MatterService: Normalizes code
    Change-Id: I62b9947c46087477971bbe0649e000fdde42ad67

--- src/main/java/cl/votainteligente/legislativo/service/MatterService.java
+++ src/main/java/cl/votainteligente/legislativo/service/MatterService.java
@@ -5,14 +5,8 @@ import cl.votainteligente.legislativo.exception.ServiceException;
 import cl.votainteligente.legislativo.model.Matter;
 
 public interface MatterService {
-
 	Matter newMatter(Matter matter) throws ServiceException;
-
 	Matter getById(Long id) throws ServiceException;
-
-	Page<Matter> findByName(String name, int pageNumber, int resultsPerPage)
-			throws ServiceException;
-
-	Page<Matter> getAll(int pageNumber, int resultsPerPage)
-			throws ServiceException;
+	Page<Matter> findByName(String name, int pageNumber, int resultsPerPage) throws ServiceException;
+	Page<Matter> getAll(int pageNumber, int resultsPerPage) throws ServiceException;
 }
