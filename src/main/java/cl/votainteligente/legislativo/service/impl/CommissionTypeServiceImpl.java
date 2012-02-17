8623698dc9af06e8c4445a7c2bfc8c7690b3d3a8
Jose Gonzalez Quintana <jgonzalezq@votainteligente.cl>
2012-02-17 15:05:39 -0300

    CommissionTypeService: Normalizes code
    Change-Id: I2ca3ae8a19ed4866027b525434061ed1121c02eb

--- src/main/java/cl/votainteligente/legislativo/service/impl/CommissionTypeServiceImpl.java
+++ src/main/java/cl/votainteligente/legislativo/service/impl/CommissionTypeServiceImpl.java
@@ -1,32 +1,29 @@
 package cl.votainteligente.legislativo.service.impl;
 
-import java.util.List;
-
-import javax.persistence.Query;
-
-import org.springframework.stereotype.Service;
-
 import cl.votainteligente.legislativo.exception.ServiceException;
 import cl.votainteligente.legislativo.model.CommissionType;
 import cl.votainteligente.legislativo.service.CommissionTypeService;
 import cl.votainteligente.legislativo.service.EntityManagerService;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import javax.persistence.Query;
+
 @Service
-public class CommissionTypeServiceImpl extends EntityManagerService implements
-		CommissionTypeService {
+public class CommissionTypeServiceImpl extends EntityManagerService implements CommissionTypeService {
 
 	@Override
 	public CommissionType getById(Long id) throws ServiceException {
 		return getEntityManager().find(CommissionType.class, id);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public List<CommissionType> getAll() throws ServiceException {
-		Query query = getEntityManager().createQuery(
-				"select p from CommissionType p");
-		List<CommissionType> list = query.getResultList();
-		return list;
+		Query query = getEntityManager().createQuery("select p from CommissionType p");
+		List<CommissionType> commissionTypeList = query.getResultList();
+		return commissionTypeList;
 	}
 
 }
