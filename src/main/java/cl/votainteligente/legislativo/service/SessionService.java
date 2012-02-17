c2267a3241bdf8834501982faab67bd5bc5f91c9
Jose Gonzalez Quintana <jgonzalezq@votainteligente.cl>
2012-02-17 16:50:32 -0300

    SessionService: Normalizes code
    Change-Id: I97ee784b54c31e61154bcb9f02a4450ce5d95fe1

--- src/main/java/cl/votainteligente/legislativo/service/SessionService.java
+++ src/main/java/cl/votainteligente/legislativo/service/SessionService.java
@@ -1,26 +1,17 @@
 package cl.votainteligente.legislativo.service;
 
-import java.util.Date;
-
 import cl.votainteligente.legislativo.common.Page;
 import cl.votainteligente.legislativo.exception.ServiceException;
 import cl.votainteligente.legislativo.model.Session;
 import cl.votainteligente.legislativo.model.DO.SessionDO;
 import cl.votainteligente.legislativo.model.DO.SessionDetailedDO;
 
+import java.util.Date;
+
 public interface SessionService {
-
-	Page<SessionDO> getAllSessionDO(int page, int perPage)
-			throws ServiceException;
-
+	Page<SessionDO> getAllSessionDO(int page, int perPage) throws ServiceException;
 	SessionDetailedDO getSessionDetailedDO(Long id) throws ServiceException;
-
 	Session getSession(Long id) throws ServiceException;
-
-	Page<SessionDO> getByDateRange(Date from, Date to, int page, int perPage)
-			throws ServiceException;
-
-	Page<SessionDO> getByLegislature(Long id, int page, int perPage)
-			throws ServiceException;
-
+	Page<SessionDO> getByDateRange(Date from, Date to, int page, int perPage) throws ServiceException;
+	Page<SessionDO> getByLegislature(Long id, int page, int perPage) throws ServiceException;
 }
--- src/main/java/cl/votainteligente/legislativo/service/impl/SessionServiceImpl.java
+++ src/main/java/cl/votainteligente/legislativo/service/impl/SessionServiceImpl.java
@@ -1,14 +1,5 @@
 package cl.votainteligente.legislativo.service.impl;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import javax.persistence.Query;
-import javax.persistence.TemporalType;
-
-import org.springframework.stereotype.Service;
-
 import cl.votainteligente.legislativo.common.Page;
 import cl.votainteligente.legislativo.exception.ServiceException;
 import cl.votainteligente.legislativo.model.Session;
@@ -17,33 +8,37 @@ import cl.votainteligente.legislativo.model.DO.SessionDetailedDO;
 import cl.votainteligente.legislativo.service.EntityManagerService;
 import cl.votainteligente.legislativo.service.SessionService;
 
-@Service
-public class SessionServiceImpl extends EntityManagerService implements
-		SessionService {
+import org.springframework.stereotype.Service;
 
-	@SuppressWarnings("unchecked")
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Query;
+import javax.persistence.TemporalType;
+
+@Service
+public class SessionServiceImpl extends EntityManagerService implements SessionService {
+
 	@Override
-	public Page<SessionDO> getAllSessionDO(int page, int perPage)
-			throws ServiceException {
-		Query query = getEntityManager().createQuery(
-				"select s from Session s");
+	public Page<SessionDO> getAllSessionDO(int page, int perPage) throws ServiceException {
+		Query query = getEntityManager().createQuery("select s from Session s");
 		query.setFirstResult((page - 1) * perPage);
 		query.setMaxResults(perPage);
-		List<SessionDO> listDO = new ArrayList<SessionDO>();
-		for (Session session : (List<Session>) query
-				.getResultList()) {
-			listDO.add(session.asDomainObject());
+		List<SessionDO> sessionDOList = new ArrayList<SessionDO>();
+
+		for (Session session : (List<Session>) query.getResultList()) {
+			sessionDOList.add(session.asDomainObject());
 		}
-		Query queryCount = getEntityManager().createQuery(
-				"select count(s) from Session s");
+
+		Query queryCount = getEntityManager().createQuery("select count(s) from Session s");
 		Long total = (Long) queryCount.getSingleResult();
 
-		return new Page<SessionDO>(listDO, page, perPage, total);
+		return new Page<SessionDO>(sessionDOList, page, perPage, total);
 	}
 
 	@Override
-	public SessionDetailedDO getSessionDetailedDO(Long id)
-			throws ServiceException {
+	public SessionDetailedDO getSessionDetailedDO(Long id) throws ServiceException {
 		return getSession(id).asDetailedDomainObject();
 	}
 
@@ -52,49 +47,41 @@ public class SessionServiceImpl extends EntityManagerService implements
 		return getEntityManager().find(Session.class, id);
 	}
 
-	@SuppressWarnings("unchecked")
-	public Page<SessionDO> getByDateRange(Date from, Date to, int page,
-			int perPage) throws ServiceException {
-		Query query = getEntityManager().createQuery(
-				"select s from Session s where s.date between ? and ?");
+	public Page<SessionDO> getByDateRange(Date from, Date to, int page, int perPage) throws ServiceException {
+		Query query = getEntityManager().createQuery("select s from Session s where s.date between ? and ?");
 		query.setParameter(1, from, TemporalType.DATE);
 		query.setParameter(2, to, TemporalType.DATE);
 		query.setFirstResult((page - 1) * perPage);
 		query.setMaxResults(perPage);
-		List<SessionDO> listDO = new ArrayList<SessionDO>();
-		for (Session session : (List<Session>) query
-				.getResultList()) {
-			listDO.add(session.asDomainObject());
+		List<SessionDO> sessionDOList = new ArrayList<SessionDO>();
+
+		for (Session session : (List<Session>) query.getResultList()) {
+			sessionDOList.add(session.asDomainObject());
 		}
-		Query queryCount = getEntityManager()
-				.createQuery(
-						"select count(s) from Session s where s.date between ? and ?");
+
+		Query queryCount = getEntityManager().createQuery("select count(s) from Session s where s.date between ? and ?");
 		queryCount.setParameter(1, from, TemporalType.DATE);
 		queryCount.setParameter(2, to, TemporalType.DATE);
 		Long total = (Long) queryCount.getSingleResult();
 
-		return new Page<SessionDO>(listDO, page, perPage, total);
+		return new Page<SessionDO>(sessionDOList, page, perPage, total);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
-	public Page<SessionDO> getByLegislature(Long id, int page,
-			int perPage) throws ServiceException {
-		Query query = getEntityManager().createQuery(
-				"select s from Session s where s.legislature = ?");
+	public Page<SessionDO> getByLegislature(Long id, int page, int perPage) throws ServiceException {
+		Query query = getEntityManager().createQuery("select s from Session s where s.legislature = ?");
 		query.setParameter(1, id);
 		query.setFirstResult((page - 1) * perPage);
 		query.setMaxResults(perPage);
-		List<SessionDO> listDO = new ArrayList<SessionDO>();
-		for (Session session : (List<Session>) query
-				.getResultList()) {
-			listDO.add(session.asDomainObject());
+		List<SessionDO> sessionDOList = new ArrayList<SessionDO>();
+
+		for (Session session : (List<Session>) query.getResultList()) {
+			sessionDOList.add(session.asDomainObject());
 		}
-		Query queryCount = getEntityManager()
-				.createQuery(
-						"select count(s) from Session s where s.legislature = ?");
+
+		Query queryCount = getEntityManager().createQuery("select count(s) from Session s where s.legislature = ?");
 		queryCount.setParameter(1, id);
 		Long total = (Long) queryCount.getSingleResult();
-		return new Page<SessionDO>(listDO, page, perPage, total);
+		return new Page<SessionDO>(sessionDOList, page, perPage, total);
 	}
 }
