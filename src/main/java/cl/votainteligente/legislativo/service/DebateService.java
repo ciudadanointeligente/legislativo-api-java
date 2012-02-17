397c2443f16dab91da1bf9b6efcca770f87bb5a9
Jose Gonzalez Quintana <jgonzalezq@votainteligente.cl>
2012-02-17 15:53:29 -0300

    DebateService: Normalizes code
    Change-Id: I6d53a42cbf62561d17f3318524ba0f89e134a2c4

--- src/main/java/cl/votainteligente/legislativo/service/DebateService.java
+++ src/main/java/cl/votainteligente/legislativo/service/DebateService.java
@@ -1,7 +1,5 @@
 package cl.votainteligente.legislativo.service;
 
-import java.util.Date;
-
 import cl.votainteligente.legislativo.common.Page;
 import cl.votainteligente.legislativo.exception.ServiceException;
 import cl.votainteligente.legislativo.model.Bill;
@@ -9,17 +7,12 @@ import cl.votainteligente.legislativo.model.Debate;
 import cl.votainteligente.legislativo.model.DO.DebateDO;
 import cl.votainteligente.legislativo.model.DO.DebateDetailedDO;
 
+import java.util.Date;
+
 public interface DebateService {
 	Debate saveDebate(Debate d) throws ServiceException;
-
 	Debate getDebate(long id) throws ServiceException;
-
 	DebateDetailedDO getDebateDetailedDO(long id) throws ServiceException;
-
-	Page<DebateDO> getDebateByBill(Bill bill, int page, int perPage)
-			throws ServiceException;
-
-	Page<DebateDO> getDebateByDateRange(Date from, Date to, int page,
-			int perPage) throws ServiceException;
-
+	Page<DebateDO> getDebateByBill(Bill bill, int page, int perPage) throws ServiceException;
+	Page<DebateDO> getDebateByDateRange(Date from, Date to, int page, int perPage) throws ServiceException;
 }
