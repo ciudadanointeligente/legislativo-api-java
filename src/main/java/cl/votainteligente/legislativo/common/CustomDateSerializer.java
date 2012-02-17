8d4605514bcc91910d3286cf513c10d8b063808c
Juan Pablo Perez T <jpperez@ciudadanointeligente.cl>
2012-02-17 11:38:00 -0300

    Commons: Normalizes code
    Change-Id: I4aee85928ba8f647950e910df018545ad8393fe4

--- src/main/java/cl/votainteligente/legislativo/common/CustomDateSerializer.java
+++ src/main/java/cl/votainteligente/legislativo/common/CustomDateSerializer.java
@@ -1,22 +1,21 @@
 package cl.votainteligente.legislativo.common;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import cl.votainteligente.legislativo.ApplicationProperties;
 
 import org.codehaus.jackson.JsonGenerator;
 import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.map.JsonSerializer;
 import org.codehaus.jackson.map.SerializerProvider;
 
-import cl.votainteligente.legislativo.ApplicationProperties;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CustomDateSerializer extends JsonSerializer<Date> {
 
-	public void serialize(Date value, JsonGenerator gen, SerializerProvider arg2)
-			throws IOException, JsonProcessingException {
-		SimpleDateFormat formatter = new SimpleDateFormat(
-				ApplicationProperties.getProperty("controller.date.format"));
+	@Override
+	public void serialize(Date value, JsonGenerator gen, SerializerProvider arg2) throws IOException, JsonProcessingException {
+		SimpleDateFormat formatter = new SimpleDateFormat(ApplicationProperties.getProperty("controller.date.format"));
 		String formattedDate = formatter.format(value);
 		gen.writeString(formattedDate);
 	}
