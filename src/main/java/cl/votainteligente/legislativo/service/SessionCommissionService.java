4a3c010290ad72868d91786996a9f480b68e1363
Jose Gonzalez Quintana <jgonzalezq@votainteligente.cl>
2012-02-17 16:27:28 -0300

    SessionCommissionService: Normalizes code
    Change-Id: I65e281648812bc53695874565064b00220b9f0a2

--- src/main/java/cl/votainteligente/legislativo/service/SessionCommissionService.java
+++ src/main/java/cl/votainteligente/legislativo/service/SessionCommissionService.java
@@ -1,26 +1,17 @@
 package cl.votainteligente.legislativo.service;
 
-import java.util.Date;
-
 import cl.votainteligente.legislativo.common.Page;
 import cl.votainteligente.legislativo.exception.ServiceException;
 import cl.votainteligente.legislativo.model.SessionCommission;
 import cl.votainteligente.legislativo.model.DO.SessionCommissionDO;
 import cl.votainteligente.legislativo.model.DO.SessionCommissionDetailedDO;
 
+import java.util.Date;
+
 public interface SessionCommissionService {
-
-	Page<SessionCommissionDO> getAllSessionCommissionDO(int page, int perPage)
-			throws ServiceException;
-
-	SessionCommissionDetailedDO getSessionCommissionDetailedDO(Long id)
-			throws ServiceException;
-
+	Page<SessionCommissionDO> getAllSessionCommissionDO(int page, int perPage) throws ServiceException;
+	SessionCommissionDetailedDO getSessionCommissionDetailedDO(Long id) throws ServiceException;
 	SessionCommission getSessionCommission(Long id) throws ServiceException;
-
-	Page<SessionCommissionDO> getByDateRange(Date from, Date to, int page,
-			int perPage) throws ServiceException;
-
-	Page<SessionCommissionDO> getByLegislature(Long id, int page, int perPage)
-			throws ServiceException;
+	Page<SessionCommissionDO> getByDateRange(Date from, Date to, int page, int perPage) throws ServiceException;
+	Page<SessionCommissionDO> getByLegislature(Long id, int page, int perPage) throws ServiceException;
 }
